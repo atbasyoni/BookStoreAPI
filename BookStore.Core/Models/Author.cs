e6feb141b43879a1c19eb725c3ee76f95dd79a98
Taha <Taha@DESKTOP-B8GPB9D>
2024-05-13 19:30:40 +0300

    Add project files.

---
+++ BookStore.Core/Models/Author.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BookStore.Core.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Bio { get; set; }
+        public string? Image { get; set; }
+        public List<Book>? Books { get; set; }
+    }
+}
---
+++ BookStore.EF/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BookStore.Core.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.EF.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+        public override DbSet<ApplicationUser> Users { get; set; }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
