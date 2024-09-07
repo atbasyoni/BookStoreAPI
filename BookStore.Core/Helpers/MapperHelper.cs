87d9a648280b51a0035431f0bc4fad9937ce0fa5
Ahmed Taha Basyoni <atbasyoni98@gmail.com>
2024-09-07 04:23:37 +0300

    [Add] MapperHelper & ValiditionBehavior

---
+++ BookStore.Core/Helpers/MapperHelper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper.QueryableExtensions;
+using AutoMapper;
+
+namespace BookStore.Core.Helper
+{
+    public static class MapperHelper
+    {
+        public static IMapper Mapper { get; set; }
+
+        public static IEnumerable<TResult> Map<TResult>(this IQueryable source)
+        {
+            return source.ProjectTo<TResult>(Mapper.ConfigurationProvider);
+        }
+
+        public static TResult MapOne<TResult>(this object source)
+        {
+            return Mapper.Map<TResult>(source);
+        }
+    }
+}
